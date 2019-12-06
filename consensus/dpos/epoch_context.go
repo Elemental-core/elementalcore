1b7e0ba0b96107c983ee1548a136862c65b890d8
Elemental-core <elementalcore01@yahoo.com>
2019-12-06 11:48:36 +0800

    Second submission

---
+++ consensus/dpos/epoch_context.go
@@ -0,0 +1,237 @@
+package dpos
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"math/big"
+	"math/rand"
+	"sort"
+
+	"github.com/Elemental-core/elementalcore/common"
+	"github.com/Elemental-core/elementalcore/core/state"
+	"github.com/Elemental-core/elementalcore/core/types"
+	"github.com/Elemental-core/elementalcore/crypto"
+	"github.com/Elemental-core/elementalcore/log"
+	"github.com/Elemental-core/elementalcore/trie"
+)
+
+type EpochContext struct {
+	TimeStamp   int64
+	DposContext *types.DposContext
+	statedb     *state.StateDB
+}
+
+// countVotes
+func (ec *EpochContext) countVotes() (votes map[common.Address]*big.Int, err error) {
+	votes = map[common.Address]*big.Int{}
+	candidateTrie := ec.DposContext.CandidateTrie()
+	voteTrie := ec.DposContext.VoteTrie()
+
+	statedb := ec.statedb
+
+	iterCandidate := trie.NewIterator(candidateTrie.NodeIterator(nil))
+	existCandidate := iterCandidate.Next()
+	if !existCandidate {
+		return votes, errors.New("no candidates")
+	}
+	for existCandidate {
+		candidate := iterCandidate.Value
+		candidateAddr := common.BytesToAddress(candidate)
+		score, ok := votes[candidateAddr]
+		if !ok {
+			score = new(big.Int)
+		}
+		weight := statedb.GetBalance(candidateAddr)
+
+		vote , err := voteTrie.TryGet(candidate)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if bytes.Equal(vote, []byte{}) {
+			score.Add(score, weight)
+		}else{
+			//获取当前候选者的币龄
+			score.Add(score, new(big.Int).Mul(new(big.Int).SetInt64(BytesToInt(vote)),weight))
+		}
+
+		votes[candidateAddr] = score
+		existCandidate = iterCandidate.Next()
+	}
+	return votes, nil
+}
+
+func (ec *EpochContext) kickoutValidator(epoch int64) error {
+	validators, err := ec.DposContext.GetValidators()
+	if err != nil {
+		return fmt.Errorf("failed to get validator: %s", err)
+	}
+	if len(validators) == 0 {
+		return errors.New("no validator could be kickout")
+	}
+
+	epochDuration := epochInterval
+	// First epoch duration may lt epoch interval,
+	// while the first block time wouldn't always align with epoch interval,
+	// so caculate the first epoch duartion with first block time instead of epoch interval,
+	// prevent the validators were kickout incorrectly.
+	if ec.TimeStamp-timeOfFirstBlock < epochInterval {
+		epochDuration = ec.TimeStamp - timeOfFirstBlock
+	}
+
+	needKickoutValidators := sortableAddresses{}
+	for _, validator := range validators {
+		key := make([]byte, 8)
+		binary.BigEndian.PutUint64(key, uint64(epoch))
+		key = append(key, validator.Bytes()...)
+		cnt := int64(0)
+		if cntBytes := ec.DposContext.MintCntTrie().Get(key); cntBytes != nil {
+			cnt = int64(binary.BigEndian.Uint64(cntBytes))
+		}
+		if cnt < epochDuration/blockInterval/ maxValidatorSize /2 {
+			// not active validators need kickout
+			needKickoutValidators = append(needKickoutValidators, &sortableAddress{validator, big.NewInt(cnt)})
+		}
+	}
+	// no validators need kickout
+	needKickoutValidatorCnt := len(needKickoutValidators)
+	if needKickoutValidatorCnt <= 0 {
+		return nil
+	}
+	sort.Sort(sort.Reverse(needKickoutValidators))
+
+	candidateCount := 0
+	iter := trie.NewIterator(ec.DposContext.CandidateTrie().NodeIterator(nil))
+	for iter.Next() {
+		candidateCount++
+		if candidateCount >= needKickoutValidatorCnt+safeSize {
+			break
+		}
+	}
+
+	for i, validator := range needKickoutValidators {
+		// ensure candidate count greater than or equal to safeSize
+		if candidateCount <= safeSize {
+			log.Info("No more candidate can be kickout", "prevEpochID", epoch, "candidateCount", candidateCount, "needKickoutCount", len(needKickoutValidators)-i)
+			return nil
+		}
+
+		if err := ec.DposContext.KickoutCandidate(validator.address); err != nil {
+			return err
+		}
+		// if kickout success, candidateCount minus 1
+		candidateCount--
+		log.Info("Kickout candidate", "prevEpochID", epoch, "candidate", validator.address.String(), "mintCnt", validator.weight.String())
+	}
+	return nil
+}
+
+func (ec *EpochContext) lookupValidator(now int64) (validator common.Address, err error) {
+	validator = common.Address{}
+	offset := now % epochInterval
+	if offset%blockInterval != 0 {
+		return common.Address{}, ErrInvalidMintBlockTime
+	}
+	offset /= blockInterval
+
+	validators, err := ec.DposContext.GetValidators()
+	if err != nil {
+		return common.Address{}, err
+	}
+	validatorSize := len(validators)
+	if validatorSize == 0 {
+		return common.Address{}, errors.New("failed to lookup validator")
+	}
+	offset %= int64(validatorSize)
+	return validators[offset], nil
+}
+
+func (ec *EpochContext) tryElect(genesis, parent *types.Header) error {
+	genesisEpoch := genesis.Time.Int64() / epochInterval
+	prevEpoch := parent.Time.Int64() / epochInterval
+	currentEpoch := ec.TimeStamp / epochInterval
+
+	prevEpochIsGenesis := prevEpoch == genesisEpoch
+	if prevEpochIsGenesis && prevEpoch < currentEpoch {
+		prevEpoch = currentEpoch - 1
+	}
+
+	prevEpochBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(prevEpochBytes, uint64(prevEpoch))
+	iter := trie.NewIterator(ec.DposContext.MintCntTrie().PrefixIterator(prevEpochBytes))
+	for i := prevEpoch; i < currentEpoch; i++ {
+		// if prevEpoch is not genesis, kickout not active candidate
+		if !prevEpochIsGenesis && iter.Next() {
+			if err := ec.kickoutValidator(prevEpoch); err != nil {
+				return err
+			}
+		}
+		votes, err := ec.countVotes()
+		if err != nil {
+			return err
+		}
+		candidates := sortableAddresses{}
+		for candidate, cnt := range votes {
+			candidates = append(candidates, &sortableAddress{candidate, cnt})
+		}
+		if len(candidates) < safeSize {
+			return errors.New("too few candidates")
+		}
+		sort.Sort(candidates)
+		if len(candidates) > maxValidatorSize {
+			candidates = candidates[:maxValidatorSize]
+		}
+
+		// shuffle candidates
+		seed := int64(binary.LittleEndian.Uint32(crypto.Keccak512(parent.Hash().Bytes()))) + i
+		r := rand.New(rand.NewSource(seed))
+		for i := len(candidates) - 1; i > 0; i-- {
+			j := int(r.Int31n(int32(i + 1)))
+			candidates[i], candidates[j] = candidates[j], candidates[i]
+		}
+		sortedValidators := make([]common.Address, 0)
+		for _, candidate := range candidates {
+			sortedValidators = append(sortedValidators, candidate.address)
+		}
+
+		epochTrie, _ := types.NewEpochTrie(common.Hash{}, ec.DposContext.DB())
+		ec.DposContext.SetEpoch(epochTrie)
+		ec.DposContext.SetValidators(sortedValidators)
+		log.Info("Come to new epoch", "prevEpoch", i, "nextEpoch", i+1)
+	}
+	return nil
+}
+
+type sortableAddress struct {
+	address common.Address
+	weight  *big.Int
+}
+type sortableAddresses []*sortableAddress
+
+func (p sortableAddresses) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+func (p sortableAddresses) Len() int      { return len(p) }
+func (p sortableAddresses) Less(i, j int) bool {
+	if p[i].weight.Cmp(p[j].weight) < 0 {
+		return false
+	} else if p[i].weight.Cmp(p[j].weight) > 0 {
+		return true
+	} else {
+		return p[i].address.String() < p[j].address.String()
+	}
+}
+func BytesToInt(b []byte) int64 {
+	bytesBuffer := bytes.NewBuffer(b)
+
+	var x int64
+	binary.Read(bytesBuffer, binary.BigEndian, &x)
+
+	return int64(x)
+}
+//整形转换成字节
+func IntToBytes(n int64) []byte {
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.BigEndian, &n)
+	return bytesBuffer.Bytes()
+
+}
